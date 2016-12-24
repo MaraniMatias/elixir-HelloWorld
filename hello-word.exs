5be17862d0f27ac0d7bd9894d299dcf88ce3a271
Marani Matias Ezequiel <maranimatias@gmail.com>
2016-12-24 17:26:24 -0300

    add bibliography

--- hello-word.exs
+++ hello-word.exs
@@ -1,5 +1,6 @@
 IO.puts "Hello world from Elixir...\n"
 
+# https://oylenshpeegul.wordpress.com/2015/10/25/hello-elixir/
 greet = fn s -> IO.puts "Hello, #{s}!" end
 
 if length(System.argv) == 0 do
