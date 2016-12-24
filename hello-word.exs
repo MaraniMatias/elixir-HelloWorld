fdefb81eb2d6704ec7b3206a7915f2143d72f760
Marani Matias Ezequiel <maranimatias@gmail.com>
2016-12-24 17:22:39 -0300

    add function and if-else

--- hello-word.exs
+++ hello-word.exs
@@ -1,1 +1,9 @@
-IO.puts "Hello world from Elixir"
+IO.puts "Hello world from Elixir...\n"
+
+greet = fn s -> IO.puts "Hello, #{s}!" end
+
+if length(System.argv) == 0 do
+    greet.("Try: \n elixir hello-word.exs Elixir")
+else
+    Enum.each(System.argv, greet)
+end
