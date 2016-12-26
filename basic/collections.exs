2459355c9053cec61461506f796fd1703d01da92
Marani Matias Ezequiel <maranimatias@gmail.com>
2016-12-26 16:08:09 -0300

    add tuplas

--- basic/collections.exs
+++ basic/collections.exs
@@ -14,4 +14,18 @@ IO.puts [h|t] = [3.14, :pie, "Apple"] # [3.14, :pie, "Apple"]
 IO.puts h                             # 3.14
 IO.puts t                             # [:pie, "Apple"]
 
+# Tuples
+IO.puts {3.14, :pie, "Apple"}
+# return
+File.read("path/to/existing/file")  # {:ok, "... contents ..."}
+File.read("path/to/unknown/file")   # {:error, :enoent}
+
+IO.puts [foo: "bar", hello: "world"]        # [foo: "bar", hello: "world"]
+IO.puts [{:foo, "bar"}, {:hello, "world"}]  # [foo: "bar", hello: "world"]
+# The three characteristics of keyword lists highlight their importance
+## Keys are atoms.
+## Keys are ordered.
+## Keys are not unique.
+
+# Maps
 
