40b123fc637a1c618b1bb4afb5623715a57a9fdd
Marani Matias Ezequiel <maranimatias@gmail.com>
2016-12-28 18:26:22 -0300

    strings

---
+++ basic/strings.exs
@@ -0,0 +1,24 @@
+IO.puts <<104,101,108,108,111>> # "hello" <<bytes>>
+
+# Char Lists
+char_list = 'hello'
+# [hd|tl] = char_list
+# IO.puts {hd, tl} # {104, 'ello'}
+
+Enum.reduce(char_list, "", fn char, acc -> 
+  acc <> to_string(char) <> ","
+end) # "104,101,108,108,111,"
+
+# Graphemes and Codepoints
+string = "\u0061\u0301" # "á"
+IO.puts String.codepoints string # ["a", "'"]
+IO.puts String.graphemes string  # ["á"]
+
+# String Functions
+# http://elixir-lang.org/docs/stable/elixir/String.html
+IO.puts String.length "Hello"             # 5
+IO.puts String.replace("Hello", "e", "a") # "Hallo"
+IO.puts String.replace "Hello", "e", "a"  # "Hallo"
+
+IO.puts String.duplicate("Oh my ", 3)     # "Oh my Oh my Oh my "
+IO.puts String.split("Hello World", " ")  # ["Hello", "World"]
