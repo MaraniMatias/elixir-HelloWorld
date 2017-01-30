b05bb819abafb4cd6bce6d6eea7c6381614b6de6
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-01-30 20:27:45 -0300

    update script

--- elixir-install.sh
+++ elixir-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "Add Repo.."
-wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
+sudo apt install inotify-tools
 sudo apt install esl-erlang
 sudo apt install elixir
 sudo apt install erlang
+mix local.hex
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
