d058a594c6eba54b3b50f5d7397d146924ffe77b
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-03-31 23:53:01 -0300

    new examples for stings

--- basic/strings.exs
+++ basic/strings.exs
@@ -5,12 +5,27 @@ IO.puts is_list("Example")          # false
 IO.puts is_binary("Example")        # true
 IO.puts <<"Example">> === "Example" # true
 
+IO.puts List.to_string('elixir')              # "elixir"
+IO.puts String.to_char_list("elixir") #'elixir'
+
+pass_through = ~S"This is a {#msg}, she said.\n  This is only a {#msg}."
+IO.puts pass_through
+#  s (for binary string)
+#  c (for character list)
+#  r (for regular expression)
+#  w (to produce a list split into words by whitespace).
+IO.puts ~w/Elixir is great!/ # ["Elixir", "is", "great!"]
+
 # Char Lists
 char_list = 'hello'
 # [hd|tl] = char_list
 # IO.puts {hd, tl} # {104, 'ello'}
 
-Enum.reduce(char_list, "", fn char, acc -> 
+# @@@ Warning @@@
+# Character lists are slower to work with and take up 
+# more memory than strings, so they shouldn’t be your first choice
+
+Enum.reduce(char_list, "", fn char, acc ->
   acc <> to_string(char) <> ","
 end) # "104,101,108,108,111,"
 
@@ -27,3 +42,18 @@ IO.puts String.replace "Hello", "e", "a"  # "Hallo"
 
 IO.puts String.duplicate("Oh my ", 3)     # "Oh my Oh my Oh my "
 IO.puts String.split("Hello World", " ")  # ["Hello", "World"]
+
+a = "ir"
+b = "elix#{a}"
+IO.puts b
+a = "ir-2"
+IO.puts b
+
+y = {4, 5, 6}
+IO.puts "y is now #{inspect y}"
+
+ms = """Hi,
+I am 
+Multiline Strings
+"""
+IO.puts ms
